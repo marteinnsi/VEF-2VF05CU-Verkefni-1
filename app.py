17317836cfc9c4ab6eb49fc35fdb7f3150f56372
marteinnsi216 <marteinnsi216@nemi.tskoli.is>
2020-01-13 17:14:56 +0000

    Setja inn undirsidur og static skjol

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask import abort
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def route_index():
+    return "Hello world"
+
+@app.route("/http")
+def route_http():
+    return app.send_static_file("html/http.html")
+
+@app.route("/wolf")
+def route_wolf():
+    return app.send_static_file("html/wolf.html")
+
+@app.route("/static/<dir>/<file>")
+def route_static(dir, file):
+    if dir in ["css", "img"]:
+        return app.send_static_file(f"{dir}/{file}")
+    abort(404)
